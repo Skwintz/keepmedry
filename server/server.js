aca3ec473d690de5505bbaabd2109d840e055221
Paul Nispel <pnispel@gmail.com>
2013-06-15 16:16:08 -0500

    removed email call from server startup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@
 Meteor.startup(function () {
   	process.env.MAIL_URL = 'smtp://app16323043%40heroku.com:R-7HISkxtCqqXK6FPMz5FQ@smtp.mandrillapp.com:587';
 
-	  // In your client code: asynchronously send an email
-	Meteor.call('sendEmail',
-	            '4023040146@vtext.com',
-	            'bob@example.com',
-	            'Hello from Meteor!',
-	            'This is a test of Email.send.');
+	//   // In your client code: asynchronously send an email
+	// Meteor.call('sendEmail',
+	//             '4023040146@vtext.com',
+	//             'bob@example.com',
+	//             'Hello from Meteor!',
+	//             'This is a test of Email.send.');
 });
 
 // In your server code: define a method that the client can call
