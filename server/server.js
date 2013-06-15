2ae4e80d8b8aedb4be3cc3d429a9477f239f262d
Paul Nispel <pnispel@gmail.com>
2013-06-15 15:13:44 -0500

    added txt

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 
-Meteor.startup( function () {
-	Email.send({
-	  to: '4023040146@vtext.com',
-	  from: 'pnispel@gmail.com',
-	  subject: 'IT GON\' RAIN!',
-	  text: 'Get cho rain jacket on, trick!',
-	  replyTo: 'pnispel@gmail.com',
-	});
+// In your server code: define a method that the client can call
+Meteor.methods({
+  sendEmail: function (to, from, subject, text) {
+    check([to, from, subject, text], [String]);
+
+    // Let other method calls from the same client start running,
+    // without waiting for the email sending to complete.
+    this.unblock();
+
+    Email.send({
+      to: to,
+      from: from,
+      subject: subject,
+      text: text
+    });
+  }
 });
+
+// In your client code: asynchronously send an email
+Meteor.call('sendEmail',
+            'pnispel@gmail.com',
+            'bob@example.com',
+            'Hello from Meteor!',
+            'This is a test of Email.send.');
